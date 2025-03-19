469ce112778eeb9d00279ad5d0286bcce5c74d8d
alwin <alwin@alto.id>
2025-03-20 04:33:03 +0700

    update docker file

--- workspace/case-service/.docker/config/build-backup.sh
+++ workspace/case-service/.docker/config/build-backup.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [[ ! -d "/app/vendor" || -z $(ls -A "/app/vendor") ]]; then
+	echo "[build.sh: go mod vendor for $APP_NAME]"
+	cd /app && go mod vendor
+fi
+
+# Run compiled service
+go run -mod=vendor /app/main.go "$@"
+
+
 #!/bin/sh
 
 set -e
