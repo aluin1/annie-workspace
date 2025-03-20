b10c098b8a003b6ce3d23082ddf2c32d78b23ad5
aluin1 <alwinalwin57@gmail.com>
2025-03-20 10:18:05 -0500

    update logic

--- workspace/dashboard-case-service/html/forms/config.js
+++ workspace/dashboard-case-service/html/forms/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    URL_INSERT_CASE: 'http://localhost:8081/insertCase',
-    URL_GET_CASE: 'http://localhost:8081/dataCase',
-    URL_GET_TOKEN: 'http://localhost:8081/getToken', 
+    URL_INSERT_CASE: 'https://annievipbe.ismartdds.com/insertCase',
+    URL_GET_CASE: 'https://annievipbe.ismartdds.com/dataCase',
+    URL_GET_TOKEN: 'https://annievipbe.ismartdds.com/getToken', 
     CLIENT_ID:'79d4270a98d241999fa7e54e015b51ab',
     CLIENT_SECRET:'22106803111c4cdda620875ed4779129' ,
-    URL_UPLOAD_FILE:'http://localhost:8081/uploadFiles'
+    URL_UPLOAD_FILE:'https://annievipbe.ismartdds.com/uploadFiles'
 };
 
 // const CONFIG = {
